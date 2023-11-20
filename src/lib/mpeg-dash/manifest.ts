9d953d3cd81d70c370933f18c5e960a3d90cd4f5
Steve Taylor <steve.taylor@streamotion.com.au>
2023-11-21 01:05:56 +1100

    Download and store the init segment, adding more spaghetti code in the process. Video doesn’t play yet.

---
+++ src/lib/mpeg-dash/manifest.ts
@@ -0,0 +1,421 @@
+import {map, Observable} from 'rxjs'
+import {ajax} from 'rxjs/ajax'
+
+export function getDashManifest(url: string): Observable<XMLDocument> {
+    return ajax<string>({
+        url,
+        method: 'GET',
+        responseType: 'text'
+    }).pipe(
+        map(res => res.response),
+        map(xml => new DOMParser().parseFromString(xml, 'application/xml'))
+    )
+}
+
+export type SegmentTemplate = {
+    duration: number // seconds
+    timescale: number
+    media: string // e.g. "$RepresentationID$/$RepresentationID$_$Number$.m4v"
+    startNumber: number
+    initialization: string // "$RepresentationID$/$RepresentationID$_0.m4v"
+}
+
+type BaseRepresentation = {
+    id: string
+    codecs: string
+    bandwidth: number
+}
+
+export type VideoRepresentation = BaseRepresentation & {
+    width: number
+    height: number
+    frameRate: number
+    sar: string // aspect ratio, e.g. "1:1"
+    scanType: 'progressive' | 'interlaced' // interlaced was a guess
+}
+
+type AudioChannelConfiguration = {
+    value: number
+}
+
+export type AudioRepresentation = BaseRepresentation & {
+    audioSamplingRate: number
+    audioChannelConfiguration: AudioChannelConfiguration
+}
+
+type BaseDashAdaptationSet = {
+    mimeType: string
+    subsegmentAlignment: boolean
+    subsegmentStartsWithSAP: number
+    segmentTemplate: SegmentTemplate
+}
+
+export type DashVideoAdaptationSet = BaseDashAdaptationSet & {
+    contentType: 'video'
+    par: string // aspect ratio
+    representations: VideoRepresentation[]
+}
+
+export type DashAudioAdaptationSet = BaseDashAdaptationSet & {
+    contentType: 'audio'
+    representations: AudioRepresentation[]
+}
+
+export type DashAdaptationSet = DashVideoAdaptationSet | DashAudioAdaptationSet
+
+type DashPeriod = {
+    adaptationSets: DashAdaptationSet[]
+}
+
+type DashManifest = {
+    type: 'static' | 'dynamic'
+    mediaPresentationDuration: number
+    minBufferTime: number
+    profiles: string
+    baseUrl: string
+    periods: DashPeriod[]
+}
+
+export function parseDashManifest(manifest: XMLDocument): DashManifest {
+    const {documentElement} = manifest
+    const typeAttribute = documentElement.getAttribute('type')
+    const mediaPresentationDurationAttribute = documentElement.getAttribute(
+        'mediaPresentationDuration'
+    )
+    const minBufferTimeAttribute = documentElement.getAttribute('minBufferTime')
+    const profilesAttribute = documentElement.getAttribute('profiles')
+
+    if (typeAttribute !== 'static' && typeAttribute !== 'dynamic') {
+        throw new Error('Invalid type in MPD element: ' + typeAttribute)
+    }
+
+    if (!mediaPresentationDurationAttribute) {
+        throw new Error('Missing mediaPresentationDuration in MPD element')
+    }
+
+    if (!minBufferTimeAttribute) {
+        throw new Error('Missing minBufferTime in MPD element')
+    }
+
+    if (!profilesAttribute) {
+        throw new Error('Missing profiles in MPD element')
+    }
+
+    const mediaPresentationDuration = parseSeconds(
+        mediaPresentationDurationAttribute
+    )
+    const minBufferTime = parseSeconds(minBufferTimeAttribute)
+
+    const periods: DashPeriod[] = []
+
+    for (const periodElement of documentElement.getElementsByTagName(
+        'Period'
+    )) {
+        periods.push(parsePeriod(periodElement))
+    }
+
+    const baseUrl =
+        documentElement
+            .getElementsByTagName('BaseURL')[0]
+            ?.textContent?.trim() ?? './'
+
+    return {
+        type: typeAttribute,
+        mediaPresentationDuration,
+        minBufferTime,
+        profiles: profilesAttribute,
+        baseUrl: baseUrl === './' ? '' : baseUrl,
+        periods
+    }
+}
+
+function parsePeriod(periodElement: Element): DashPeriod {
+    const adaptationSets: DashAdaptationSet[] = []
+
+    for (const adaptationSetElement of periodElement.getElementsByTagName(
+        'AdaptationSet'
+    )) {
+        adaptationSets.push(parseAdaptationSet(adaptationSetElement))
+    }
+
+    return {
+        adaptationSets
+    }
+}
+
+function parseAdaptationSet(adaptationSetElement: Element): DashAdaptationSet {
+    const contentTypeAttribute =
+        adaptationSetElement.getAttribute('contentType')
+
+    switch (contentTypeAttribute) {
+        case 'video':
+            return parseVideoAdaptationSet(adaptationSetElement)
+        case 'audio':
+            return parseAudioAdaptationSet(adaptationSetElement)
+        default:
+            throw new Error(
+                'Invalid contentType in AdaptationSet: ' + contentTypeAttribute
+            )
+    }
+}
+
+function parseVideoAdaptationSet(
+    adaptationSetElement: Element
+): DashVideoAdaptationSet {
+    const par = adaptationSetElement.getAttribute('par')
+
+    if (!par) {
+        throw new Error('Missing par in AdaptationSet')
+    }
+
+    const representations: VideoRepresentation[] = []
+
+    for (const representationElement of adaptationSetElement.getElementsByTagName(
+        'Representation'
+    )) {
+        representations.push(parseVideoRepresentation(representationElement))
+    }
+
+    return {
+        ...parseBaseAdaptationSet(adaptationSetElement),
+        contentType: 'video',
+        par,
+        representations
+    }
+}
+
+function parseAudioAdaptationSet(
+    adaptationSetElement: Element
+): DashAudioAdaptationSet {
+    const representations: AudioRepresentation[] = []
+
+    for (const representationElement of adaptationSetElement.getElementsByTagName(
+        'Representation'
+    )) {
+        representations.push(parseAudioRepresentation(representationElement))
+    }
+
+    return {
+        ...parseBaseAdaptationSet(adaptationSetElement),
+        contentType: 'audio',
+        representations
+    }
+}
+
+function parseBaseAdaptationSet(
+    adaptationSetElement: Element
+): BaseDashAdaptationSet {
+    const mimeType = adaptationSetElement.getAttribute('mimeType')
+    const subsegmentAlignment =
+        adaptationSetElement.getAttribute('subsegmentAlignment') === 'true'
+    const subsegmentStartsWithSAP = parseInt(
+        adaptationSetElement.getAttribute('subsegmentStartsWithSAP') as string
+    )
+
+    if (!mimeType) {
+        throw new Error('Missing mimeType in AdaptationSet')
+    }
+
+    if (!Number.isFinite(subsegmentStartsWithSAP)) {
+        throw new Error(
+            'Invalid subsegmentStartsWithSAP in AdaptationSet: ' +
+                subsegmentStartsWithSAP
+        )
+    }
+
+    const [segmentTemplateElement] =
+        adaptationSetElement.getElementsByTagName('SegmentTemplate')
+
+    if (!segmentTemplateElement) {
+        throw new Error('Missing SegmentTemplate from AdaptationSet')
+    }
+
+    const segmentTemplate = parseSegmentTemplate(segmentTemplateElement)
+
+    return {
+        mimeType,
+        segmentTemplate,
+        subsegmentAlignment,
+        subsegmentStartsWithSAP
+    }
+}
+
+function parseSegmentTemplate(
+    segmentTemplateElement: Element
+): SegmentTemplate {
+    const duration = parseInt(
+        segmentTemplateElement.getAttribute('duration') as string
+    )
+    const timescale = parseInt(
+        segmentTemplateElement.getAttribute('timescale') as string
+    )
+    const media = segmentTemplateElement.getAttribute('media')
+    const startNumber = parseInt(
+        segmentTemplateElement.getAttribute('startNumber') as string
+    )
+    const initialization = segmentTemplateElement.getAttribute('initialization')
+
+    if (!Number.isFinite(duration)) {
+        throw new Error('Invalid duration in SegmentTemplate: ' + duration)
+    }
+
+    if (!Number.isFinite(timescale)) {
+        throw new Error('Invalid timescale in SegmentTemplate: ' + timescale)
+    }
+
+    if (!media) {
+        throw new Error('Missing media in SegmentTemplate')
+    }
+
+    if (!Number.isFinite(startNumber)) {
+        throw new Error(
+            'Invalid startNumber in SegmentTemplate: ' + startNumber
+        )
+    }
+
+    if (!initialization) {
+        throw new Error('Missing initialization in SegmentTemplate')
+    }
+
+    return {
+        duration,
+        timescale,
+        media,
+        startNumber,
+        initialization
+    }
+}
+
+function parseVideoRepresentation(
+    representationElement: Element
+): VideoRepresentation {
+    // width, height, frameRate, sar, scanType
+    const width = parseInt(
+        representationElement.getAttribute('width') as string
+    )
+    const height = parseInt(
+        representationElement.getAttribute('height') as string
+    )
+    const framerateAttribute = representationElement.getAttribute('frameRate')
+    const sar = representationElement.getAttribute('sar')
+    const scanType = representationElement.getAttribute('scanType')
+
+    if (!Number.isFinite(width)) {
+        throw new Error('Invalid width in Representation: ' + width)
+    }
+
+    if (!Number.isFinite(height)) {
+        throw new Error('Invalid height in Representation: ' + height)
+    }
+
+    if (!framerateAttribute) {
+        throw new Error('Missing frameRate in Representation')
+    }
+
+    if (!sar) {
+        throw new Error('Missing sar in Representation')
+    }
+
+    if (scanType !== 'progressive' && scanType !== 'interlaced') {
+        throw new Error('Invalid scanType in Representation: ' + scanType)
+    }
+
+    const [framerateNumerator, framerateDenominator] = (
+        framerateAttribute?.split('/') ?? []
+    ).map(number => parseInt(number))
+
+    if (framerateNumerator === undefined) {
+        throw new Error('Missing or invalid frameRate in Representation')
+    }
+
+    const frameRate =
+        framerateDenominator === undefined
+            ? framerateNumerator
+            : framerateNumerator / framerateDenominator
+
+    return {
+        ...parseBaseRepresentation(representationElement),
+        width,
+        height,
+        frameRate,
+        sar,
+        scanType
+    }
+}
+
+function parseAudioRepresentation(
+    representationElement: Element
+): AudioRepresentation {
+    const audioSamplingRate = parseInt(
+        representationElement.getAttribute('audioSamplingRate') as string
+    )
+
+    if (!Number.isFinite(audioSamplingRate)) {
+        throw new Error(
+            'Invalid audioSamplingRate in Representation: ' + audioSamplingRate
+        )
+    }
+
+    const [audioChannelConfigurationElement] =
+        representationElement.getElementsByTagName('AudioChannelConfiguration')
+
+    if (!audioChannelConfigurationElement) {
+        throw new Error('Missing AudioChannelConfigurationin Representation')
+    }
+
+    return {
+        ...parseBaseRepresentation(representationElement),
+        audioSamplingRate,
+        audioChannelConfiguration: parseAudioChannelConfiguration(
+            audioChannelConfigurationElement
+        )
+    }
+}
+
+function parseBaseRepresentation(
+    representationElement: Element
+): BaseRepresentation {
+    const id = representationElement.getAttribute('id')
+    const codecs = representationElement.getAttribute('codecs')
+    const bandwidth = parseInt(
+        representationElement.getAttribute('bandwidth') as string
+    )
+
+    if (!id) {
+        throw new Error('Representation missing id')
+    }
+
+    if (!codecs) {
+        throw new Error('Representation missing codecs')
+    }
+
+    if (!Number.isFinite(bandwidth)) {
+        throw new Error('Invalid bandwidth in Representation: ' + bandwidth)
+    }
+
+    return {
+        id,
+        codecs,
+        bandwidth
+    }
+}
+
+function parseAudioChannelConfiguration(
+    audioChannelConfigurationElement: Element
+): AudioChannelConfiguration {
+    const value = parseInt(
+        audioChannelConfigurationElement.getAttribute('value') as string
+    )
+
+    if (!Number.isFinite(value)) {
+        throw new Error('Invalid value in AudioChannelConfiguration')
+    }
+
+    return {
+        value
+    }
+}
+
+function parseSeconds(time: string): number {
+    return parseFloat(time.slice(2, -1))
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,151 @@
 <script lang="ts">
-    import {getDashManifest, parseDashManifest} from '$lib/mpeg-dash'
+    import {
+        type DashVideoAdaptationSet,
+        getDashManifest,
+        parseDashManifest,
+        type VideoRepresentation
+    } from '$lib/mpeg-dash'
+    import {map, Observable} from 'rxjs'
+    import {ajax} from 'rxjs/ajax'
     import {onMount} from 'svelte'
 
-    const DASH_MANIFEST_URL = '/api/video/big-buck-bunny/index.mpd'
+    const VIDEO_BASE_URL = '/api/video/big-buck-bunny'
+    const DASH_MANIFEST_URL = `${VIDEO_BASE_URL}/index.mpd`
+
+    let video: HTMLVideoElement
 
     onMount(() => {
-        const subscription = getDashManifest(DASH_MANIFEST_URL).subscribe({
-            next(value) {
-                // TODO: Download chunks and feed them into the buffer
-                console.log(parseDashManifest(value))
-            }
-        })
+        const subscription = getDashManifest(DASH_MANIFEST_URL)
+            .pipe(map(parseDashManifest))
+            .subscribe({
+                next(manifest) {
+                    console.log(manifest)
+
+                    // TODO: Download chunks and feed them into the buffer
+
+                    const [period] = manifest.periods
+
+                    if (!period) {
+                        return
+                    }
+
+                    const adaptationSet = period.adaptationSets.find(
+                        (
+                            adaptationSet
+                        ): adaptationSet is DashVideoAdaptationSet =>
+                            adaptationSet.contentType === 'video'
+                    )
+
+                    if (!adaptationSet) {
+                        return
+                    }
+
+                    const [representation] = adaptationSet.representations
+
+                    if (!representation) {
+                        return
+                    }
+
+                    // Number of segments after the init segment
+                    const segmentCount =
+                        Math.ceil(
+                            manifest.mediaPresentationDuration /
+                                (adaptationSet.segmentTemplate.duration /
+                                    adaptationSet.segmentTemplate.timescale)
+                        ) - 1
+                    const firstSegmentIndex =
+                        adaptationSet.segmentTemplate.startNumber
+                    const lastSegmentIndex = firstSegmentIndex + segmentCount
+
+                    function getInitSegmentUrl(
+                        adaptationSet: DashVideoAdaptationSet,
+                        representation: VideoRepresentation
+                    ) {
+                        return `${VIDEO_BASE_URL}${
+                            manifest.baseUrl
+                        }/${adaptationSet.segmentTemplate.initialization
+                            .replaceAll('$RepresentationID$', representation.id)
+                            .replaceAll(
+                                '$Bandwidth$',
+                                `${representation.bandwidth}`
+                            )}`
+                    }
+
+                    function getSegmentUrl(
+                        adaptationSet: DashVideoAdaptationSet,
+                        representation: VideoRepresentation,
+                        index: number
+                    ) {
+                        return `${VIDEO_BASE_URL}${
+                            manifest.baseUrl
+                        }/${adaptationSet.segmentTemplate.media
+                            .replaceAll('$RepresentationID$', representation.id)
+                            .replaceAll(
+                                '$Bandwidth$',
+                                `${representation.bandwidth}`
+                            )
+                            .replaceAll('$Number$', `${index}`)}`
+                    }
+
+                    function loadArrayBuffer(
+                        url: string
+                    ): Observable<ArrayBuffer> {
+                        return ajax<ArrayBuffer>({
+                            url,
+                            method: 'GET',
+                            responseType: 'arraybuffer'
+                        }).pipe(map(res => new Uint8Array(res.response)))
+                    }
+
+                    function loadInitSegment(
+                        adaptationSet: DashVideoAdaptationSet,
+                        representation: VideoRepresentation
+                    ) {
+                        return loadArrayBuffer(
+                            getInitSegmentUrl(adaptationSet, representation)
+                        )
+                    }
+
+                    function loadSegment(
+                        adaptationSet: DashVideoAdaptationSet,
+                        representation: VideoRepresentation,
+                        index: number
+                    ) {
+                        return loadArrayBuffer(
+                            getSegmentUrl(adaptationSet, representation, index)
+                        )
+                    }
+
+                    // timescale: units per second (this could typically be frames per second)
+
+                    // duration: duration / timescale = the duration of a segment (which means duration could typically be the number of frames in a segment)
+
+                    const mediaSource = new MediaSource()
+
+                    mediaSource.addEventListener(
+                        'sourceopen',
+                        () => {
+                            const sourceBufferType = `${adaptationSet.mimeType}; codecs="${representation.codecs}"`
+                            const sourceBuffer =
+                                mediaSource.addSourceBuffer(sourceBufferType)
+
+                            // Load the first segment
+                            loadInitSegment(
+                                adaptationSet,
+                                representation
+                            ).subscribe(initSegment => {
+                                sourceBuffer.appendBuffer(initSegment)
+                                // video.play()
+                            })
+
+                            // TODO: Load the remaining segments in sequence
+                        },
+                        {once: true}
+                    )
+
+                    video.src = URL.createObjectURL(mediaSource)
+                }
+            })
 
         return () => subscription.unsubscribe()
     })
@@ -18,7 +153,7 @@
 
 <h1>DASH video player</h1>
 <div>
-    <video controls />
+    <video bind:this={video} controls />
 </div>
 
 <style>
